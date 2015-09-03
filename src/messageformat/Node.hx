aedb0463f0fd16124e42fd77ece2df5296968fb8
James <gotnospirit@gmail.com>
2015-09-03 15:06:44 +0200

    declaration de NodeExpr dans Node.hx

--- src/messageformat/Node.hx
+++ src/messageformat/Node.hx
@@ -1,5 +1,29 @@
 package messageformat;
 
+class NodeExpr extends Node
+{
+    var type:String;
+    var expr:Dynamic;
+
+    public function new(type:String, expr:Dynamic)
+    {
+        super();
+
+        this.type = type;
+        this.expr = expr;
+    }
+
+    override public function format(output:StringBuf, mf:MessageFormat, ?params:Map<String, Dynamic>)
+    {
+        var formatter = mf.get(type);
+
+        if (null != formatter)
+        {
+            formatter(expr, output, params);
+        }
+    }
+}
+
 class Node
 {
     var children:Array<Node>;
